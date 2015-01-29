0db57e77427dcd0926536f93a9a3ede74619a5dc
Aaron Young <m_aaron@yahoo.com>
2015-01-28 18:17:14 -0600

    Starting investigating AST-centered approach

--- test.cpp
+++ test.cpp
@@ -1,50 +1,78 @@
 
 #pragma warning( disable : 4503 )
 
-#include "parse.h"
-#include "delimited_list.h"
-//#include "ast.h"
+#include <cctype>
+#include <string>
+
+namespace parsing
+{
+    struct int32_decimal
+    {
+        int value;
+
+        template <typename iter_t>
+        bool parse(iter_t& start, iter_t& end)
+        {
+            iter_t tmp = start;
+            iter_t::value_type c;
+            value = 0;
+
+            while (tmp != end && isdigit(c = *tmp++))
+                value = value * 10 + (c & 0x0f);
+
+            return (tmp > start);
+        }
+    };
+
+    struct comma_delim
+    {
+
+        template <typename iter_t>
+        bool parse(iter_t& start, iter_t& end)
+        {
+
+        }
+    };
+
+    struct a_and_b
+    {
+        int32_decimal a;
+        int32_decimal b;
+
+        template <typename iter_t>
+        bool parse(iter_t& start, iter_t& end)
+        {
+            return seq(a, b);
+        }
+    };
+
+    struct a_or_b
+    {
+        int32_decimal a, b;
+
+        template <typename iter_t>
+        bool parse(iter_t& start, iter_t& end)
+        {
+            return alt(a, b);
+        }
+    };
+
+    struct a_star
+    {
+        int a;
+
+        template <typename iter_t>
+        bool parse(iter_t& start, iter_t& end)
+        {
+            return star(a);
+        }
+    };
+}
 
 int main()
 {
-    using namespace parse;
-    using namespace parse::operators;
-    using namespace parse::terminals;
-
-    auto a0 = terminals::u<'a'>();
-    auto comma = terminals::u<','>();
-    terminals::u<'+'> plus;
-    terminals::u<'-'> minus;
-    terminals::u<'.'> dot;
-    terminals::u<' '> sp;
-    terminals::u<'='> eq;
-    terminals::u<'&'> amp;
-    terminals::u<'$'> dollar;
-    terminals::u<'*'> star;
-    terminals::u<'@'> at;
-    terminals::u<'!'> bang;
-    terminals::u<'%'> pct;
-    terminals::u<'/'> fslash;
-
-    typedef decltype(a0[_0] % fslash) a1;
-    typedef decltype(a1()[_0] % pct) a2;
-    typedef decltype(a2()[_0] % bang) a3;
-    typedef decltype(a3()[_0] % at) a4;
-    typedef decltype(a4()[_0] % star) a5;
-    typedef decltype(a5()[_0] % dollar) a6;
-    typedef decltype(a6()[_0] % amp) a7;
-    typedef decltype(a7()[_0] % eq) a8;
-    typedef decltype(a8()[_0] % sp) a9;
-    typedef decltype(a9()[_0] % dot) a10;
-    typedef decltype(a10()[_0] % minus) a11;
-    typedef decltype(a11()[_0] % plus) a12;
-    typedef decltype(a12()[_0] % comma) a13;
-
-    typedef a13 p;
-
-    std::string data("a,a+a+a,a-a+a-a");
-    auto ast = p::make_ast(data);
-    auto begin = data.begin();
-    auto end = data.end();
-    bool valid = p::parse_from(begin, end, ast);
+    parsing::int32_decimal i;
+    std::string data("23473498");
+    
+    bool valid = i.parse(data.begin(), data.end());
 }
