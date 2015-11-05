12e4832781f6918584df7063084f2b6f604b06cc
Aaron Young <m_aaron@yahoo.com>
2015-11-05 08:01:24 -0600

    Removed duplicate files

--- ConsoleApplication1/Parse.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using Functional;
-
-namespace Parse
-{
-    public delegate Either<Success<T>, Failure<T>> Parser<T>(IParseInput<T> input);
-    public delegate Either<Success<T, V>, Failure<T>> Parser<T, V>(IParseInput<T> input);
-}
